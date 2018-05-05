77bc720142fbcf48a87bf2c338ec2f541925cb65
arilsmn <arilsmn@github.com>
2018-05-05 20:48:42 +0700

    CS Modul 9

--- JHotel_Android_AriaLesmana/app/src/main/java/com/example/arialesmana/jhotel_android_arialesmana/MenuListAdapter.java
+++ JHotel_Android_AriaLesmana/app/src/main/java/com/example/arialesmana/jhotel_android_arialesmana/MenuListAdapter.java
@@ -42,7 +42,7 @@ public class MenuListAdapter extends BaseExpandableListAdapter {
         if (convertView == null) {
             LayoutInflater infalInflater = (LayoutInflater) this._context
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = infalInflater.inflate(R.layout.child, null);
+            convertView = infalInflater.inflate(R.layout.layout_room, null);
         }
 
         TextView txtListChild = (TextView) convertView
@@ -80,7 +80,7 @@ public class MenuListAdapter extends BaseExpandableListAdapter {
         if (convertView == null) {
             LayoutInflater infalInflater = (LayoutInflater) this._context
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = infalInflater.inflate(R.layout.child, null);
+            convertView = infalInflater.inflate(R.layout.layout_room, null);
         }
 
         TextView lblListHeader = (TextView) convertView
